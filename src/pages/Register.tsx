1ab324bc863a36c7a03afb74b44945a80c209788
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-04-19 12:02:59 +0530

    Developed Register component

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,6 +3,8 @@ import HeaderCompo from "./HeaderCompo";
 import '../App.css'
 
 export default function Register() {
+
+
     return (
         <>
             <div className="conrainer">
@@ -18,87 +20,25 @@ export default function Register() {
             </div>
 
             <div className="container">
-                <div className="raw text-center">
+                <div className="raw">
 
 
 
 
                     <body className="bg-body-tertiary">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="d-none">
-                            <symbol id="check2" viewBox="0 0 16 16">
-                                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-                            </symbol>
-                            <symbol id="circle-half" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-                            </symbol>
-                            <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-                                <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
-                                <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
-                            </symbol>
-                            <symbol id="sun-fill" viewBox="0 0 16 16">
-                                <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                            </symbol>
-                        </svg>
+                        
 
-                        <div className="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-                            <button className="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-                                id="bd-theme"
-                                type="button"
-                                aria-expanded="false"
-                                data-bs-toggle="dropdown"
-                                aria-label="Toggle theme (auto)">
-                                <svg className="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-                                <span className="visually-hidden" id="bd-theme-text">Toggle theme</span>
-                            </button>
-                            <ul className="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-                                <li>
-                                    <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-                                        <svg className="bi me-2 opacity-50" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-                                        Light
-                                        <svg className="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-                                        <svg className="bi me-2 opacity-50" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-                                        Dark
-                                        <svg className="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type="button" className="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-                                        <svg className="bi me-2 opacity-50" width="1em" height="1em"><use href="#circle-half"></use></svg>
-                                        Auto
-                                        <svg className="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-
-
-
-
-
-
-
-
+                        
                         <div className="container">
                             <main>
                                 <div className="py-5 text-center">
-                                    <img className="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-                                    <h2>Checkout form</h2>
-                                    <p className="lead">Below is an example form built entirely with Bootstraps form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+                                    <h2>User Registeration Form</h2>
                                 </div>
 
 
-                                <div className="row g-5">
-
-
-
-                                    
+                                <div className="row g-5">                                    
                                   
                                     <div className="col-md-7 col-lg-8">
-                                        <h4 className="mb-3">Billing address</h4>
                                         <form className="needs-validation" noValidate>
                                             <div className="row g-3">
                                                 <div className="col-sm-6">
@@ -117,19 +57,28 @@ export default function Register() {
                                                     </div>
                                                 </div>
 
-                                                <div className="col-12">
-                                                    <label htmlFor="username" className="form-label">Username</label>
-                                                    <div className="input-group has-validation">
-                                                        <span className="input-group-text">@</span>
-                                                        <input type="text" className="form-control" id="username" placeholder="Username" required />
-                                                        <div className="invalid-feedback">
-                                                            Your username is required.
-                                                        </div>
+
+
+
+                                                <div className="col-sm-6">
+                                                    <label htmlFor="email" className="form-label">Phone Number 1 </label>
+                                                    <input type="email" className="form-control" id="email" placeholder="0771234567" />
+                                                    <div className="invalid-feedback">
+                                                        Please enter a valid email address for shipping updates.
                                                     </div>
                                                 </div>
 
+                                                 <div className="col-sm-6">
+                                                    <label htmlFor="email" className="form-label">Phone Number 2 <span className="text-body-secondary">(Optional)</span></label>
+                                                    <input type="email" className="form-control" id="email" placeholder="0771234567" />
+                                                    <div className="invalid-feedback">
+                                                        Please enter a valid email address for shipping updates.
+                                                    </div>
+                                                </div>
+
+                                                                                  
                                                 <div className="col-12">
-                                                    <label htmlFor="email" className="form-label">Email <span className="text-body-secondary">(Optional)</span></label>
+                                                    <label htmlFor="email" className="form-label">Email </label>
                                                     <input type="email" className="form-control" id="email" placeholder="you@example.com" />
                                                     <div className="invalid-feedback">
                                                         Please enter a valid email address for shipping updates.
@@ -137,7 +86,7 @@ export default function Register() {
                                                 </div>
 
                                                 <div className="col-12">
-                                                    <label htmlFor="address" className="form-label">Address</label>
+                                                    <label htmlFor="address" className="form-label">Address Line1</label>
                                                     <input type="text" className="form-control" id="address" placeholder="1234 Main St" required />
                                                     <div className="invalid-feedback">
                                                         Please enter your shipping address.
@@ -145,54 +94,62 @@ export default function Register() {
                                                 </div>
 
                                                 <div className="col-12">
-                                                    <label htmlFor="address2" className="form-label">Address 2 <span className="text-body-secondary">(Optional)</span></label>
-                                                    <input type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
+                                                    <label htmlFor="address2" className="form-label">Address Line2 <span className="text-body-secondary">(Optional)</span></label>
+                                                    <input type="text" className="form-control" id="address2" placeholder="Colombo road" />
                                                 </div>
 
-                                                <div className="col-md-5">
-                                                    <label htmlFor="country" className="form-label">Country</label>
+                                                <div className="col-md-6">
+                                                    <label htmlFor="country" className="form-label">City</label>
                                                     <select className="form-select" id="country" required>
                                                         <option value="">Choose...</option>
                                                         <option>United States</option>
                                                     </select>
                                                     <div className="invalid-feedback">
-                                                        Please select a valid country.
+                                                        Please select valid City.
                                                     </div>
                                                 </div>
 
-                                                <div className="col-md-4">
-                                                    <label htmlFor="state" className="form-label">State</label>
+                                                <div className="col-md-5">
+                                                    <label htmlFor="state" className="form-label">District</label>
                                                     <select className="form-select" id="state" required>
                                                         <option value="">Choose...</option>
                                                         <option>California</option>
                                                     </select>
                                                     <div className="invalid-feedback">
-                                                        Please provide a valid state.
+                                                        Please provide District.
                                                     </div>
                                                 </div>
 
                                                 <div className="col-md-3">
-                                                    <label htmlFor="zip" className="form-label">Zip</label>
-                                                    <input type="text" className="form-control" id="zip" placeholder="" required />
+                                                    <label htmlFor="state" className="form-label">Year Of Birth</label>
+                                                    <select className="form-select" id="state" required>
+                                                        <option value="">Choose...</option>
+                                                        <option>California</option>
+                                                    </select>
                                                     <div className="invalid-feedback">
-                                                        Zip code required.
+                                                        Please provide Year Of Birth.
                                                     </div>
                                                 </div>
+                                                <div className="col-md-3">
+                                                    <label htmlFor="state" className="form-label">Gender</label>
+                                                    <select className="form-select" id="state" required>
+                                                        <option value="">Choose...</option>
+                                                        <option>California</option>
+                                                    </select>
+                                                    <div className="invalid-feedback">
+                                                        Please provide Gender.
+                                                    </div>
+                                                </div>
+
+                                                
                                             </div>
 
-                                            <hr className="my-4" />
+                                            <hr className="my-7" />
 
-                                            <div className="form-check">
-                                                <input type="checkbox" className="form-check-input" id="same-address" />
-                                                <label className="form-check-label" htmlFor="same-address">Shipping address is the same as my billing address</label>
-                                            </div>
 
-                                            <div className="form-check">
-                                                <input type="checkbox" className="form-check-input" id="save-info" />
-                                                <label className="form-check-label" htmlFor="save-info">Save this information for next time</label>
-                                            </div>
 
-                                            <hr className="my-4" />
+
+
 
                                             <h4 className="mb-3">Payment</h4>
 
